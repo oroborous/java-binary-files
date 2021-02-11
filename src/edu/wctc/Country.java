1e2fec377127fce31852477b8eb286097ea17046
Stacy Read <stacy.read.007@gmail.com>
2021-02-10 21:39:01 -0600

    Initial commit

---
+++ src/edu/wctc/Country.java
@@ -0,0 +1,53 @@
+package edu.wctc;
+
+import java.io.Serializable;
+
+public class Country implements Serializable {
+    private String continent;
+    private String name;
+    private String capital;
+    private int population;
+    private int area;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+}
---
+++ src/edu/wctc/Main.java
@@ -0,0 +1,97 @@
+package edu.wctc;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static List<Country> countryList = new ArrayList<>();
+
+    private static void controlBreak() {
+        int count = 0;
+        String currentContinent = countryList.get(0).getContinent();
+
+        // print header for table
+        System.out.printf("%-20s%-20s%n", "Continent", "Number of Countries");
+
+        for (Country country : countryList) {
+
+            if (!currentContinent.equals(country.getContinent())) {
+                // this is the first country for a new continent
+
+                // output count for previous continent
+                System.out.printf("%-20s%-20d%n", currentContinent, count);
+
+                // store the new continent
+                currentContinent = country.getContinent();
+                // reset the count
+                count = 0;
+            }
+
+            // add this country to the count for current continent
+            count++;
+        }
+    }
+
+    private static void writeCountryListObject() {
+        // Write object list as binary file
+        try (
+                FileOutputStream fos = new FileOutputStream("countries.obj");
+                ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(countryList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readCountryListObject() {
+        // Read the binary file back into list variable
+        try (
+                FileInputStream fis = new FileInputStream("countries.obj");
+                ObjectInputStream ois = new ObjectInputStream(fis);
+        ) {
+            countryList = (List<Country>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readCsvIntoList() {
+        // Read CSV into object list
+
+        try (Scanner fileInput = new Scanner(new File("countries.txt"))) {
+            while (fileInput.hasNext()) {
+                String line = fileInput.nextLine();
+                String[] columns = line.split(";");
+
+                Country country = new Country();
+                countryList.add(country);
+
+                // name, continent, capital, population, area
+                country.setName(columns[0]);
+                country.setContinent(columns[1]);
+                country.setCapital(columns[2]);
+                country.setPopulation(Integer.parseInt(columns[3]));
+                country.setArea(Integer.parseInt(columns[4]));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("PROGRAM START");
+
+//        readCsvIntoList();
+
+//        writeCountryListObject();
+
+        readCountryListObject();
+
+        System.out.println("Country list has " + countryList.size() + " countries");
+
+        System.out.println("PROGRAM END");
+    }
+}
